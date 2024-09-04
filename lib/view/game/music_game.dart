a842afdd55b363a40825e65e35a5ce195cf5bd3d
taro <tarofess@gmail.com>
2024-09-04 13:45:54 +0900

    音楽ゲームをスマホとタブレットで同じようなレイアウトに修正

--- lib/view/game/music_game.dart
+++ lib/view/game/music_game.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MusicGame extends HookWidget {
   const MusicGame({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final catScale = useState(1.0);
-    final chickenScale = useState(1.0);
-    final goatScale = useState(1.0);
-    final elephantScale = useState(1.0);
-    final dogScale = useState(1.0);
+    final pianoScale = useState(1.0);
+    final violinScale = useState(1.0);
+    final acousticScale = useState(1.0);
+    final snareScale = useState(1.0);
+    final guitarScale = useState(1.0);
 
     void animateScale(ValueNotifier<double> scale) {
       scale.value = 1.5;
@@ -26,93 +27,53 @@ class MusicGame extends HookWidget {
             image: DecorationImage(
               image: Image.asset('assets/images/music/bg_music.jpg').image,
               fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.5),
+                BlendMode.dstATop,
+              ),
             ),
           ),
         ),
-        // 中央
         Center(
-          child: GestureDetector(
-            onTap: () => animateScale(catScale),
-            child: AnimatedScale(
-              scale: catScale.value,
-              duration: const Duration(milliseconds: 200),
-              child: Image.asset(
-                'assets/images/music/piano.png',
-                width: 150,
-                height: 150,
-              ),
-            ),
-          ),
+          child: buildMusicImage('piano', pianoScale, animateScale),
         ),
-        // 左上
         Positioned(
-          top: 20,
-          left: 100,
-          child: GestureDetector(
-            onTap: () => animateScale(chickenScale),
-            child: AnimatedScale(
-              scale: chickenScale.value,
-              duration: const Duration(milliseconds: 200),
-              child: Image.asset(
-                'assets/images/music/violin.png',
-                width: 150,
-                height: 150,
-              ),
-            ),
-          ),
+          top: 0.r,
+          left: 40.r,
+          child: buildMusicImage('violin', violinScale, animateScale),
         ),
-        // 右上
         Positioned(
-          top: 20,
-          right: 100,
-          child: GestureDetector(
-            onTap: () => animateScale(goatScale),
-            child: AnimatedScale(
-              scale: goatScale.value,
-              duration: const Duration(milliseconds: 200),
-              child: Image.asset(
-                'assets/images/music/acoustic.png',
-                width: 150,
-                height: 150,
-              ),
-            ),
-          ),
+          top: 0.r,
+          right: 60.r,
+          child: buildMusicImage('acoustic', acousticScale, animateScale),
         ),
-        // 左下
         Positioned(
-          bottom: 20,
-          left: 100,
-          child: GestureDetector(
-            onTap: () => animateScale(elephantScale),
-            child: AnimatedScale(
-              scale: elephantScale.value,
-              duration: const Duration(milliseconds: 200),
-              child: Image.asset(
-                'assets/images/music/snare.png',
-                width: 150,
-                height: 150,
-              ),
-            ),
-          ),
+          bottom: 20.r,
+          left: 120.r,
+          child: buildMusicImage('snare', snareScale, animateScale),
         ),
-        // 右下
         Positioned(
-          bottom: 20,
-          right: 100,
-          child: GestureDetector(
-            onTap: () => animateScale(dogScale),
-            child: AnimatedScale(
-              scale: dogScale.value,
-              duration: const Duration(milliseconds: 200),
-              child: Image.asset(
-                'assets/images/music/guitar.png',
-                width: 150,
-                height: 150,
-              ),
-            ),
-          ),
+          bottom: 0.r,
+          right: 20.r,
+          child: buildMusicImage('guitar', guitarScale, animateScale),
         ),
       ],
+    );
+  }
+
+  Widget buildMusicImage(String fileName, ValueNotifier<double> scale,
+      Function(ValueNotifier<double>) animateScale) {
+    return GestureDetector(
+      onTap: () => animateScale(scale),
+      child: AnimatedScale(
+        scale: scale.value,
+        duration: const Duration(milliseconds: 200),
+        child: Image.asset(
+          'assets/images/music/$fileName.png',
+          width: 150.r,
+          height: 150.r,
+        ),
+      ),
     );
   }
 }
