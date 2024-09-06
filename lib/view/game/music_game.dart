c08ccccde496af6eefb23e75d77f0e36a0dfcbb8
taro <tarofess@gmail.com>
2024-09-06 11:29:10 +0900

    音楽ゲームに効果音追加

--- lib/view/game/music_game.dart
+++ lib/view/game/music_game.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class MusicGame extends HookWidget {
   const MusicGame({super.key});
@@ -10,14 +11,27 @@ class MusicGame extends HookWidget {
     final pianoScale = useState(1.0);
     final violinScale = useState(1.0);
     final acousticScale = useState(1.0);
-    final snareScale = useState(1.0);
+    final drumScale = useState(1.0);
     final guitarScale = useState(1.0);
 
-    void animateScale(ValueNotifier<double> scale) {
+    final audioPlayers = useMemoized(() => {
+          'piano': AudioPlayer(),
+          'violin': AudioPlayer(),
+          'acoustic': AudioPlayer(),
+          'drum': AudioPlayer(),
+          'guitar': AudioPlayer(),
+        });
+
+    void animateScale(ValueNotifier<double> scale, int durationMs) {
       scale.value = 1.5;
-      Future.delayed(const Duration(milliseconds: 1000), () {
+      Future.delayed(Duration(milliseconds: durationMs), () {
         scale.value = 1.0;
       });
+    }
+
+    void playSound(String instrument) async {
+      final player = audioPlayers[instrument]!;
+      await player.play(AssetSource('sounds/music/$instrument.mp3'));
     }
 
     return Stack(
@@ -35,36 +49,74 @@ class MusicGame extends HookWidget {
           ),
         ),
         Center(
-          child: buildMusicImage('piano', pianoScale, animateScale),
+          child: buildMusicImage(
+            'piano',
+            pianoScale,
+            animateScale,
+            playSound,
+            3400,
+          ),
         ),
         Positioned(
           top: 0.r,
           left: 40.r,
-          child: buildMusicImage('violin', violinScale, animateScale),
+          child: buildMusicImage(
+            'violin',
+            violinScale,
+            animateScale,
+            playSound,
+            4000,
+          ),
         ),
         Positioned(
           top: 0.r,
           right: 60.r,
-          child: buildMusicImage('acoustic', acousticScale, animateScale),
+          child: buildMusicImage(
+            'acoustic',
+            acousticScale,
+            animateScale,
+            playSound,
+            2800,
+          ),
         ),
         Positioned(
           bottom: 20.r,
           left: 120.r,
-          child: buildMusicImage('drum', snareScale, animateScale),
+          child: buildMusicImage(
+            'drum',
+            drumScale,
+            animateScale,
+            playSound,
+            4000,
+          ),
         ),
         Positioned(
           bottom: 0.r,
           right: 20.r,
-          child: buildMusicImage('guitar', guitarScale, animateScale),
+          child: buildMusicImage(
+            'guitar',
+            guitarScale,
+            animateScale,
+            playSound,
+            2000,
+          ),
         ),
       ],
     );
   }
 
-  Widget buildMusicImage(String fileName, ValueNotifier<double> scale,
-      Function(ValueNotifier<double>) animateScale) {
+  Widget buildMusicImage(
+    String fileName,
+    ValueNotifier<double> scale,
+    Function(ValueNotifier<double>, int) animateScale,
+    Function(String) playSound,
+    int durationMs,
+  ) {
     return GestureDetector(
-      onTap: () => animateScale(scale),
+      onTap: () {
+        animateScale(scale, durationMs);
+        playSound(fileName);
+      },
       child: AnimatedScale(
         scale: scale.value,
         duration: const Duration(milliseconds: 200),
