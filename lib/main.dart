f783c58c92c6d40021dfa4a89bf2e97a8486e7fc
taro <tarofess@gmail.com>
2024-09-06 12:11:04 +0900

    アプリ起動中にBGMを流すよう変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:bebikame/service/navigation_service.dart';
 import 'package:bebikame/view/game_selection_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_it/get_it.dart';
@@ -20,11 +22,25 @@ void main() {
   FlutterNativeSplash.remove();
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final audioPlayer = useMemoized(() => AudioPlayer(), []);
+
+    useEffect(() {
+      audioPlayer.setReleaseMode(ReleaseMode.loop);
+      audioPlayer.setPlayerMode(PlayerMode.lowLatency);
+      audioPlayer.setSource(AssetSource('sounds/bgm.mp3'));
+      audioPlayer.setVolume(0.5);
+      audioPlayer.resume();
+
+      return () {
+        audioPlayer.dispose();
+      };
+    }, []);
+
     return ScreenUtilInit(
       designSize: const Size(690, 360),
       minTextAdapt: true,
