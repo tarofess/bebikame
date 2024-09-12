f59463e2213a05d393a8fe28addc8a47d60a1789
taro <tarofess@gmail.com>
2024-09-12 17:35:39 +0900

    インジケーター表示時にテキストメッセージも表示するよう修正

--- lib/view/widget/loading_indicator.dart
+++ lib/view/widget/loading_indicator.dart
@@ -13,15 +13,30 @@ class LoadingIndicator extends StatelessWidget {
           height: double.infinity,
         ),
         Center(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: CircularProgressIndicator(
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: CircularProgressIndicator(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                '処理中です...',
+              ),
+            ],
           ),
         ),
       ],
