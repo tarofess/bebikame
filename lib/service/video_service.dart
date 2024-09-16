db01f4068834412cc8e0b15fd1e0aaa7a7a27417
taro <tarofess@gmail.com>
2024-09-16 11:47:54 +0900

    image_gallery_saverが古いAndroid端末で使えないため動画の保存にphoto_managerを使うよう修正

--- lib/service/video_service.dart
+++ lib/service/video_service.dart
@@ -3,10 +3,9 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 import 'package:ffmpeg_kit_flutter/return_code.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 class VideoService {
   CameraController? controller;
@@ -57,35 +56,9 @@ class VideoService {
   }
 
   Future<void> saveVideo(String videoPath) async {
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      await _saveVideoAndroid(videoPath);
-    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
-      await _saveVideoIOS(videoPath);
-    } else {
-      throw Exception('未対応のプラットフォームです。');
-    }
-  }
-
-  Future<void> _saveVideoAndroid(String videoPath) async {
     try {
-      final directory = await getApplicationDocumentsDirectory();
-      final savedVideoPath =
-          '${directory.path}/bebikame_video_${DateTime.now().millisecondsSinceEpoch}.mp4';
-      await File(videoPath).copy(savedVideoPath);
-    } catch (e) {
-      throw Exception('動画の保存に失敗しました。\n端末の容量などを確認して再度お試しください。');
-    }
-  }
-
-  Future<void> _saveVideoIOS(String videoPath) async {
-    try {
-      final result = await ImageGallerySaver.saveFile(
-        videoPath,
-        name: "bebikame_video_${DateTime.now().millisecondsSinceEpoch}.mp4",
-      );
-      if (!result['isSuccess']) {
-        throw Exception('動画の保存に失敗しました。\n端末の容量などを確認して再度お試しください。');
-      }
+      final file = File(videoPath);
+      await PhotoManager.editor.saveVideo(file);
     } catch (e) {
       throw Exception('動画の保存に失敗しました。\n端末の容量などを確認して再度お試しください。');
     }
