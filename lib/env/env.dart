1988b2c6cbc08c87745ffd3c5331dcf11ae21130
taro <tarofess@gmail.com>
2025-03-08 17:00:09 +0900

    envファイルのパス変更

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(path: 'lib/config/env/.env')
+@Envied(path: 'lib/env/.env')
 abstract class Env {
   @EnviedField(varName: 'product_id_fireworks')
   static const String fireworksGame = _Env.fireworksGame;
